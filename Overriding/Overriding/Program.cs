0f3fe110f4a5b29891dcafbcea617f59f0ae5376
coreyderosa <coreyderosa@gmail.com>
2016-11-05 16:34:11 -0600

    Overriding
    Inherit method from one method to another and use the override method in
    our main function.

---
+++ Overriding/Overriding/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Overriding
+{
+    //Plane class inheriting from Airport class
+    public class Plane : Airport
+    {
+        public override void Takeoff()
+        {
+            //Takeoff from runway
+        }
+    }
+
+    public class Airport
+    {
+        public virtual void Takeoff()
+        {
+            //Takeoff from terminal
+            //but planes dont takeoff from terminal...they take off from runway
+        }
+    }
+
+    public class Main
+    {
+        public void DoStuff()
+        {
+            Plane myPlane = new Plane();
+            myPlane.Takeoff();//this will use the overriden Airport Takeoff() method 
+                              //and will use the Plane Takeoff() method
+        }
+    }
+}
