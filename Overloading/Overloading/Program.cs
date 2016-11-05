f206211f96db8ee21125a5db91b9e446330d5bf3
coreyderosa <coreyderosa@gmail.com>
2016-11-05 16:06:42 -0600

    Overloading
    You can make 2 methods that are the same name but do different things.
    You have to have different arguments in the method for this to work.  So
    one method can have a string argument and the other has an int argument.

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDrill1
+{
+    class Program
+    {
+
+        public class Plane
+        {
+
+            public void Fly(string a)
+            {
+                Console.WriteLine("Where have you flown this year? ");
+                a = Console.ReadLine();
+                Console.WriteLine("Wow, you have flown to " + a + " this year!");
+            }
+
+            public void Fly(int numberOfFlights)
+            {
+                for (int i = 0; i < numberOfFlights; i++) ;
+            }
+        }
+
+        static void Main()
+        {
+            Plane myPlane = new Fly();
+            myPlane.Fly("Brazil");
+            myPlane.Fly(5);
+        }
+    }
+}
+
+
