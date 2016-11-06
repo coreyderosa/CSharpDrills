220a11f892b83bb2f39325242ecdba9f8079983d
coreyderosa <coreyderosa@gmail.com>
2016-11-06 13:05:39 -0700

    Interface example
    Classes can inherit from interfaces but the class must use all items
    within the interface.

---
+++ Interface1/Interface1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface1
+{
+    class Class1 : Interface1, Interface2 //can implement more than on interface
+    {
+        void Interface1.DoInterface1Stuff() //must implement all itmes of Interface1
+        {
+            throw new NotImplementedException();
+        }
+
+        void Interface2.DoInterface2Stuff() //must implement all itmes of Interface1 
+        {
+            throw new NotImplementedException();
+        }
+
+        void Interface1.DoMoreInterface1Stuff() //must implement all itmes of Interface2
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
