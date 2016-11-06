5e6b19698ce44bc86b472e2a4a45c8a70ce8090a
coreyderosa <coreyderosa@gmail.com>
2016-11-06 14:10:04 -0700

    Try/Catch/Finally
    Try some code. Catch if there is an error.  Finally run code no matter
    if there is an error or not.

---
+++ TryCatchFinally1/TryCatchFinally1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] try1 = new string[3]; //0,1,2
+                string a = try1[4]; //referring to 3 which is not in array
+            }
+            catch
+            {
+                Console.WriteLine("There is an error!");
+            }
+            finally
+            {
+                Console.WriteLine("The try/catch/finally is complete!");
+            }
+        }
+    }
+}
