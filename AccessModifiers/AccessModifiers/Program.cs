7a85c8fe16205268a86185abae5749a92fcd3bf8
coreyderosa <coreyderosa@gmail.com>
2016-11-05 20:01:13 -0600

    Access modifiers
    public has no restrictions.
    protected must be used within the class or an inherited class.
    internal only allows access within the same file.
    private cannot be accessed within another class.
    protected internal allows protected OR internal.
    
    https://msdn.microsoft.com/en-us/library/wxh6fsc7.aspx

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    ///
+    /////////////// Public ////////////////////////
+    ///
+    class Public
+    {
+        public int x; //access is not restricted
+    }
+    /// 
+    /////////////// Protected ////////////////////////
+    /// 
+    class ProtectedA
+    {
+        protected int x = 5; // Protected within this class or any class that inherits this class
+    }
+
+    class ProtectedB : ProtectedA
+    {
+        static void Main()
+        {
+            ProtectedA a = new ProtectedA();
+            ProtectedB b = new ProtectedB();
+
+            a.x = 10;//x is protected within ProtectedA so it cannot be accessed here
+            b.x = 10;//x access is granted since ProtectedB is a derived class of ProtectedA
+        }
+        
+    }
+
+}
+///
+/////////////////Internal ////////////////////
+///
+
+//File1.cs
+public class File1
+{
+    internal int x;
+}
+
+//File2.cs
+public class File2
+{
+    static void Main()
+    {
+        File1 myFile = new File1();
+        File1.x = 7; //this will throw an error since the variable x is internal in File1.cs
+    }
+    
+}
+
+///
+/////////////////// Private ///////////////////
+///
+
+ class privateA
+{
+    private int x = 5;
+
+    public int pA()
+    {
+        return x;
+    }
+}
+
+class privateAccess
+{
+    privateA a = new privateA();
+    int y = a.pA; //This will throw an error since x is has private access
+}
