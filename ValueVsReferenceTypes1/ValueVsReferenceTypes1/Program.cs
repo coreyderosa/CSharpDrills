11da0c221c00bd7ff69253bb36cca20b9ee10f9a
coreyderosa <coreyderosa@gmail.com>
2016-11-06 18:42:25 -0700

    Value vs Reference type
    value stored in the stack.
    reference stored in the heap.

---
+++ ValueVsReferenceTypes1/ValueVsReferenceTypes1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueVsReferenceTypes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7; //value type stores the data in the stack
+            int[] x = new int[7]; //reference type store the data in the heap
+        }
+    }
+}
