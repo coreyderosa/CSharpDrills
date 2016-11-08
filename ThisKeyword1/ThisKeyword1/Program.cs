32e03963333ed36ebee71e67ce36b1b34f3ad8f8
coreyderosa <coreyderosa@gmail.com>
2016-11-07 20:02:34 -0700

    This keyword
    use this to help show the variable is for this class

---
+++ ThisKeyword1/ThisKeyword1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword1
+{
+    class Program
+    {
+        public string a;
+        public string b;
+
+        static void withoutThis(string a, string b)
+        {
+            a = a; 
+            b = b;
+        }
+
+         public void withThis(string a, string b)
+        {
+            this.a = a; 
+            this.b = b;
+        }
+    }
+}
