b2094fe8ef0537b0d222bb42be70fe2ba5334fc7
coreyderosa <coreyderosa@gmail.com>
2016-11-07 20:38:47 -0700

    Serialization and creating a txt file
    Serialized a list and saved the binary in a txt file.
    Deserialized the binary within the txt file and displayed on console.

---
+++ SerializationBLOB1/SerializationBLOB1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationBLOB1
+{
+    [Serializable]
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            List<string> blobVictims = new List<string>();
+            blobVictims.Add("Gary");
+            blobVictims.Add("Carl");
+
+            string fileDir = @"D:\Documents";
+            string serializedFile = Path.Combine(fileDir, "blob.txt");
+
+            /// <summary>
+            /// This will serialize the object and create the txt file
+            /// </summary>
+            using (Stream stream = File.Open(serializedFile, FileMode.Create))
+            {
+                var binFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binFormatter.Serialize(stream, blobVictims);
+                Console.WriteLine(blobVictims);
+            }
+
+            /// <summary>
+            /// This will deserialize the object and show the blob object in the console window
+            /// </summary>
+            using (Stream stream = File.Open(serializedFile, FileMode.Open))
+            {
+                var binFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                List<string> blobOpen = (List<string>)binFormatter.Deserialize(stream);
+                Console.WriteLine(blobOpen);
+            }
+            Console.Read();
+
+        } 
+
+
+
+    }
+}
