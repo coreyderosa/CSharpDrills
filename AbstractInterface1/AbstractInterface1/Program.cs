620c0650ca78c73bbd52cb46435ef9b3bab15527
coreyderosa <coreyderosa@gmail.com>
2016-11-08 21:57:57 -0700

    Abstract class and Interfaces
    Abstract classes allow fields and implementation/functions and are
    private by default and can be changed.
    Interfaces do not allow fields or implementation and are public by
    default and cannot be changed

---
+++ AbstractInterface1/AbstractInterface1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInterface1
+{
+    
+    public abstract class Dog
+    {
+        string name = "Fido"; //Interfaces cannot contain fields
+        int dogYears = 3;
+
+        //private access by default but can have another access modifier
+        public void myDog() 
+        {
+            Console.WriteLine(name + "is " + dogYears + "years old in dog years");
+        }
+
+        
+    public interface IDog
+    {
+            string name = "Fido"; //Interfaces cannot contain fields
+            int dogYears = 3; //Interfaces cannot contain fields
+
+            //public access by default but cannot be modified
+            public void myDog()
+        {
+            Console.WriteLine(name + "is " + dogYears + "years old in dog years");
+        }
+            
+    }
+
+
+
+
+
+
+        static void Main(string[] args)
+
+        {
+
+        }
+    }
+}
