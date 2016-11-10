db8cad8a2546892e25e29cf62eeebec031ab4f46
coreyderosa <coreyderosa@gmail.com>
2016-11-09 21:38:59 -0700

    Delegates
    Not too sure how to use delegates yet but Im sure they are useful.

---
+++ Delegates1/Delegates1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    
+    public delegate void DelegateExample(string Example); 
+
+    class DelegateClass
+    {
+        public static void Main() 
+        {
+            DelegateExample ex = new DelegateExample(Derp);
+            ex("Delegates are confusing");
+        }
+              
+        public static void Derp(string delEx)
+        {
+
+            Console.WriteLine(delEx);
+
+            Console.Read();
+
+        }
+    }
+}
