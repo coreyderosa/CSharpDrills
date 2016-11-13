015b44ffbc51cc44a33be22cf97fe027ccae5581
coreyderosa <coreyderosa@gmail.com>
2016-11-12 17:21:38 -0700

    Nullable Types
    Allows a variable to have a null value.

---
+++ NullableTypes1/NullableTypes1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool? vote = null;
+
+            if (vote == true)
+            {
+                Console.WriteLine("Awesome, you voted!");
+            }
+            else if (vote == false)
+            {
+                Console.WriteLine("Why didnt vote?!");
+            }
+            else
+            {
+                Console.WriteLine("Did you vote or not?!");
+            }
+            Console.Read();
+        }
+    }
+}
