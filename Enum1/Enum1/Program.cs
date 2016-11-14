4b2f0287c3a7e424aab87bd3c5b1c54f74b2c0d3
coreyderosa <coreyderosa@gmail.com>
2016-11-13 19:03:36 -0700

    Enum
    enums allow for easier to read code and allow me to call the enum into a
    method or class easily

---
+++ Enum1/Enum1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Enums
+{
+    public static void Main()
+    {
+        Player[] players = new Player[3];
+
+        players[0] = new Player
+        {
+            playerName = "Matt Duchene",
+            nhlPosition = nhlPosition.Skater
+        };
+
+        players[1] = new Player
+        {
+            playerName = "Semyon Varlamov",
+            nhlPosition = nhlPosition.Goaile
+        };
+
+        players[2] = new Player
+        {
+            playerName = "Joe Schmoe",
+            nhlPosition = nhlPosition.Unknown
+        };
+    }
+}
+
+
+public enum nhlPosition
+{
+    Unknown,
+    Skater,
+    Goaile
+}
+
+public class noEnums
+{
+    public static void Main()
+    {
+        Player[] players = new Player[3];
+
+        players[0] = new Player
+        {
+            playerName = "Matt Duchene",
+            nhlPosition = 1
+        };
+
+        players[1] = new Player
+        {
+            playerName = "Semyon Varlamov",
+            nhlPosition = 2
+        };
+
+        players[2] = new Player
+        {
+            playerName = "Joe Schmoe",
+            nhlPosition = 0
+        };
+    }
+}
+
+
+public enum nhlPosition
+{
+    Unknown,
+    Skater,
+    Goaile
+}
